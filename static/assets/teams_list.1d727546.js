fac2deb532551b54020e36129caebbd5b4b31322
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2023-12-26 06:40:33 -0500

    challenges edit theme

---
+++ static/assets/teams_list.1d727546.js
@@ -0,0 +1,1 @@
+import{C as o,m as d}from"./index.c5c2a1d7.js";window.CTFd=o;window.Alpine=d;d.start();
--- static/assets/teams_list.9135d44e.js
+++
@@ -1,1 +0,0 @@
-import{C as o,m as d}from"./index.72421801.js";window.CTFd=o;window.Alpine=d;d.start();
